56f98898265bca68c2d25e4050257a92d85258e2
Sanjit Vignesh Shunmugarajan <SanjitVignesh.Shunmugarajan@garrettmotion.com>
2020-06-22 21:21:41 +0530

    Add project files.

---
+++ creational-patterns/Singleton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace creational_patterns
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+
+        private static object lockObject = new object();
+
+        protected Singleton()
+        {
+
+        }
+
+        public void LogMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static Singleton GetInstance()
+        {
+            if(_instance == null)
+            {
+                lock (lockObject)
+                {
+                    if(_instance == null)
+                    {
+                        _instance = new Singleton();
+                    }
+                }
+            }
+            return _instance;
+        }
+    }
+}
---
+++ design-patterns/Program.cs
@@ -0,0 +1,16 @@
+﻿using creational_patterns;
+using System;
+
+namespace design_patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton singleton = Singleton.GetInstance();
+            //singleton.
+            singleton.LogMessage("Hello");
+            Console.ReadLine();
+        }
+    }
+}
